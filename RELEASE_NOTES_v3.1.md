5ce7877c64f6fc8b66cc2cb3ef2c62708f5b367e
crystalcheung133-ux <crystalcheung133@gmail.com>
2026-07-03 22:08:04 +1000

    V4 Clean files

--- RELEASE_NOTES_v3.1.md
+++
@@ -1,3 +0,0 @@
-# RELEASE NOTES v3.1
-
-Production QA polish: Home logo blend, Shopping Directory visibility, Moments intro/emojis, Expenses first-screen layout, Trip info, Friend wording.
--- extract_places.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-const src=fs.readFileSync('script.js','utf8');
-const m=src.match(/const PLACES=(\{[\s\S]*?\});\nconst CATEGORIES=/);
-if(!m) throw new Error('no places');
-const vm=require('vm');
-const sandbox={};
-vm.runInNewContext('PLACES='+m[1], sandbox);
-fs.writeFileSync('/mnt/data/places.json', JSON.stringify(sandbox.PLACES,null,2));
